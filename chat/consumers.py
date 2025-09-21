a01845bf77a7776f6120a22d8e25ee3767b8e0d6
avinaykalyanreddy <avinaykalyanreddy@gmail.com>
2025-09-21 11:14:35 +0530

    fully chat message is created

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,49 +1,78 @@
 import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
 
-from channels.generic.websocket import  AsyncWebsocketConsumer
-from channels.db import  database_sync_to_async
+
 class ChatConsumer(AsyncWebsocketConsumer):
-
     async def connect(self):
+        self.sender_id = int(self.scope["url_route"]["kwargs"]["sender_id"])
+        self.receiver_id = int(self.scope["url_route"]["kwargs"]["receiver_id"])
 
-        self.sender_id = self.scope["url_route"]["kwargs"]["sender_id"]
-        self.receiver_id = self.scope["url_route"]["kwargs"]["receiver_id"]
-
-        print("hello world")
-        ids = sorted([self.sender_id,self.receiver_id])
-
+        ids = sorted([self.sender_id, self.receiver_id])
         self.room_group_name = f"chat_{ids[0]}_{ids[1]}"
 
-        await self.channel_layer.group_add(
+        # Join the group
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
-            self.room_group_name,self.channel_name
-        )
+        # Fetch old messages
+        messages_obj = await self.get_messages(self.sender_id, self.receiver_id)
 
-        await self.accept();
+        await self.accept()
+
+        # Send old messages
+        for message_obj in messages_obj:
+            await self.send(text_data=json.dumps({
+                "type": "previous_message",
+                "sender": message_obj.sender_id,
+                "message": message_obj.content,
+                "created_at": message_obj.created_at.isoformat()
+            }))
 
     async def disconnect(self, code):
-
-        await self.channel_layer.group_discard(self.room_group_name,self.channel_name)
-
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
-
-        data  = json.loads(text_data)
+        data = json.loads(text_data)
         message = data["message"]
 
-        await  self.channel_layer.group_send(
-            self.room_group_name,{
-                "type":"chat_message",
-                "message":message,
-                "sender":self.sender_id
+        # Save message safely
+        msg = await self.save_message(self.sender_id, self.receiver_id, message)
+
+        # Broadcast to group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "sender": msg.sender_id,
+                "message": msg.content,
+                "created_at": msg.created_at.isoformat()
             }
         )
 
-    async def chat_message(self,event):
-
-        await  self.send(text_data=json.dumps({
-            "type":event["type"],
-            "message":event["message"],
-            "sender":event["sender"]
-
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps({
+            "type": event["type"],
+            "sender": event["sender"],
+            "message": event["message"],
+            "created_at": event["created_at"]
         }))
+
+    # ---------------- ORM HELPERS ----------------
+    @database_sync_to_async
+    def get_messages(self, sender_id, receiver_id):
+        from users.models import Messages
+        return list(
+            Messages.objects.filter(
+                sender_id__in=[sender_id, receiver_id],
+                receiver_id__in=[sender_id, receiver_id]
+            ).order_by("created_at")
+        )
+
+    @database_sync_to_async
+    def save_message(self, sender_id, receiver_id, content):
+        from users.models import Messages
+        return Messages.objects.create(
+            sender_id=sender_id,
+            receiver_id=receiver_id,
+            content=content
+        )
--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,10 @@ from  . import models
 # Register your models here.
 
 admin.site.register(models.User)
+
+class AdminMessages(admin.ModelAdmin):
+
+    list_display = ["sender","receiver","content"]
+
+admin.site.register(models.Messages,AdminMessages)
 # admin.site.register(models.Friends)
