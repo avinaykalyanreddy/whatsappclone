3e43d3ae6bd6ad817e6f61b99693676d8404b860
avinaykalyanreddy <avinaykalyanreddy@gmail.com>
2025-09-21 13:09:01 +0530

    search friend feature

---
+++ chat/consumers2.py
@@ -0,0 +1,50 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+class SearchFriend(AsyncWebsocketConsumer):
+
+    async def connect(self):
+        self.sender_id = self.scope["url_route"]["kwargs"]["sender_id"]
+        self.group_name = f"searching_by_{self.sender_id}"
+
+
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+
+        await self.accept()
+        print("connect")
+    async def disconnect(self, code):
+
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        search_friend = data.get("search")
+
+        if search_friend:
+
+             friends = await  self.get_friends(search_friend)
+
+             friends_data = [{"name": i.name,"icon": i.icon} for i in friends]
+             print(friends_data)
+             await self.send(text_data=json.dumps({"friends":friends_data}))
+
+        else:
+
+            await self.send(text_data=json.dumps({"friends":[]}))
+
+
+
+
+
+    @database_sync_to_async
+    def get_friends(self,search_friend):
+
+        from users.models import User
+
+        return list(
+
+            User.objects.filter(email__icontains = search_friend).exclude(id=self.sender_id)
+        )
+
+
+
--- chat/routing.py
+++ chat/routing.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import consumers
+from . import consumers,consumers2
 
 websocket_urlpatterns = [
     # re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
 
     path("ws/chat/<int:sender_id>/<int:receiver_id>/",consumers.ChatConsumer.as_asgi()),
+    path("ws/search-friends/<int:sender_id>/",consumers2.SearchFriend.as_asgi()),
 ]
